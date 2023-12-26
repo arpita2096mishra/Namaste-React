9cf4d543798cbab51886b62aa5aee36e5f011ef1
Arpita Mishra <arpitam@cdot.in>
2023-12-26 14:05:16 +0530

    first React App

---
+++ App.js
@@ -0,0 +1,14 @@
+//const heading = React.createElement("h1", {id:"heading"}, "Hello World From React!!!");
+
+/* creating nested elements in react */
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        React.createElement("h1",{},"I am h1 Tag!!!!")
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(parent);
